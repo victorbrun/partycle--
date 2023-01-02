a55debdf4be373137afbd61090e7a358dce20df9
Ivan Flensburg <ivan.flensburg@gmail.com>
2023-01-02 15:03:49 +0100

    ads

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include "particle_generation.h"
 #include "superellipsoid.h"
 #include "coordinate_indexer.h"
 #include <eigen3/Eigen/src/Core/Matrix.h>
@@ -53,8 +52,6 @@ int main() {
 		auto c = result[ix]->get_center();
 		std::cout << "ix =" << ix << ": (x,y,z) = (" << c[0] << "," << c[1] << "," << c[2] << ")" << std::endl;
 	}
-
-	std::vector<double> test = parse_distribution("uniform(1000.98,20)")();
 
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 COMPILER 	= clang++
 FLAGS 		= -Wall -o2
-SOURCES		= main.cpp superellipsoid.cpp coordinate_indexer.cpp particle_generation.cpp
-HEADERS		= superellipsoid.h coordinate_indexer.h particle_generation.h
+SOURCES		= main.cpp superellipsoid.cpp coordinate_indexer.cpp
+HEADERS		= superellipsoid.h coordinate_indexer.h
 OUTPUT 		= partycle--
 
 compile: ${SOURCES} ${HEADERS}
