81ea2e683ee6660f136c3b6b41290ccae05166e7
Victor Brun <victor.w.brun@gmail.com>
2023-01-09 11:24:20 +0100

    Added domain string format check and component-file checks

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "program_options.hpp"
 #include <cstdlib>
 #include <iostream>
+#include <regex>
+#include <stdexcept>
+#include <filesystem>
 
 int main(int argc, char* argv[]) {
 	// Parses input arguments and stores them in file-scope varaibel	
@@ -18,8 +21,17 @@ int main(int argc, char* argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	// TODO: check that argument is correctly formated and extract domain bounds
+	// Checks that the domain definition has the right format
+	std::regex domain_string_format("^\[[0-9]+\,[0-9]+\]x\[[0-9]+\,[0-9]+\]x\[[0-9]+\,[0-9]+\]$");
+	if (!std::regex_match(domain_string, domain_string_format)) {
+		std::cerr << 
+			"particle--: invalid domain definition format. It should have the format: [ax,bx]x[ay,by]x[az,bz], where ai is lower bound for i-axis and bi is upper bound for the same axis." <<
+			"\n";
+		return EXIT_FAILURE;
+	}
+	
 
+	// Extracts the file containing the information about each component in the mixture
 	std::string components_file_name;
 	if (program_options::get_option("--component-file") != "") {
 		domain_string = program_options::get_option("--component-file");
@@ -31,7 +43,26 @@ int main(int argc, char* argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	// TODO: check that file has correct ending and verify that it exists
+	// Checking that the component-file is a csv file 
+	std::regex file_format1("*.\.csv");
+	std::regex file_format2("*.\.CSV");
+	if ( !std::regex_match(components_file_name, file_format1) && !std::regex_match(components_file_name, file_format2) ) {
+		std::cerr << "particle--: specified component-file must be csv file, i.e. have .csv or .CSV file extension." << "\n";
+		return EXIT_FAILURE;
+	}
 
+	// Checks if component-file exists
+	if (!std::filesystem::exists(components_file_name)) {
+		std::cerr << "particle--: the file: " << components_file_name << " does not exist." << "\n";
+		return EXIT_FAILURE;
+	}
+
+	// TODO: parse the domain bounds 
+	// TODO: read the file and parse the contents
+	// TODO: initiate domain and generate particles
+	// TODO: fill the domain using advancing front!!
+	// TODO: Compute contact statistics and output it in some reasonable way
+	// TODO: DONE!
+	
 	return EXIT_SUCCESS;
 }
